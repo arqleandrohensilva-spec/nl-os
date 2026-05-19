83295e8390e22ffe05c02fd2f64629902d8f9bcc
Leandro Henrique <arqleandro.hensilva@gmail.com>
2026-05-19 20:20:32 -0300

    Create use-proposta-params-types.ts

---
+++ src/hooks/use-proposta-params-types.ts
@@ -0,0 +1,13 @@
+export interface PropostaParams {
+  nome: string;
+  tipo: string;
+  cidade: string;
+  estado: string;
+  area: string;
+  objetivo: string;
+  data: string;
+  plano: string;
+  valor_executivo: string;
+  valor_completo: string;
+  validade: string;
+}
